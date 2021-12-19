f82b31a2866e9ff9e12307d21183e8569a0b3004
eyupcan-dogan <eyodohan@gmail.com>
2021-12-19 18:35:32 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, maxime.
+        Fuga, qui beatae? Ipsa similique voluptate tempore, deleniti architecto
+        esse id cupiditate et, beatae odit earum nemo iste est perferendis iusto
+        fuga inventore ducimus magnam corrupti consectetur, unde dolore cum.
+        Iusto quibusdam dicta quis? Ut pariatur placeat incidunt reprehenderit
+        impedit, vitae cum minus nemo atque optio vel amet consequatur mollitia
+        odio, repellendus voluptatem. Quae, harum nobis eaque et perspiciatis
+        officiis dolore, perferendis minus pariatur labore fugit enim cumque rem
+        blanditiis porro aspernatur minima quo quibusdam architecto dolorem.
+        Sunt ratione dicta quisquam doloribus praesentium amet vitae
+        exercitationem, asperiores, ipsam earum atque!
+      </p>
+    </section>
+  );
+};
+
+export default About;
